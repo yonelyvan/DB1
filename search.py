fef6e36046bccf25ab0c1235addaec81cb5ef387
levesques <yonelyvan@gmail.com>
2013-11-14 10:41:42 -0500

    Update search.py

--- search.py
+++ search.py
@@ -1,19 +1,18 @@
-
 from twython import Twython, TwythonError
 import os
 n = chr(34)
-name_fichero     = "peru.xml" ## fichero resultado de busquedas 
+name_fichero     = "peru.xml"     ## fichero resultado .xml de busquedas
 fichero_palabras = "datos.txt"    ## fichero de entrada de palabras
-
-
+name_database    = "bd1"
+name_table       = "peru"
 
 #####1111111111 datos de la tabla "encabezado" 
-ficheroA = open(name_fichero,"a") 
+ficheroA = open(name_fichero,"a")
 ficheroA.write("<?xml version="+n+"1.0"+n+" encoding="+n+"utf-8"+n+"?>\n" #version encoding
                +"<pma_xml_export version="+n+"1.0"+n+" xmlns:pma="+n+"http://www.phpmyadmin.net/some_doc_url/"+n+">\n" #pma_xml_export
                +"<pma:structure_schemas>\n"
-               +"<pma:database name="+n+"bd1"+n+" collation="+n+"latin1_swedish_ci"+n+" charset="+n+"latin1"+n+">\n"#database name
-               +"<pma:table name="+n+"user"+n+">\n"                       ###### table name ########### editar
+               +"<pma:database name="+n+name_database+n+" collation="+n+"latin1_swedish_ci"+n+" charset="+n+"latin1"+n+">\n"#database name
+               +"<pma:table name="+n+name_table+n+">\n" #table name 
                  
                +"CREATE TABLE `peru` ( `palabra` char(50) DEFAULT NULL,`cuenta` char(50) DEFAULT NULL,`fecha` char(50) DEFAULT NULL,`comentario` char(250) DEFAULT NULL)\n"   
                +"ENGINE=InnoDB DEFAULT CHARSET=latin1;\n"
@@ -21,11 +20,11 @@ ficheroA.write("<?xml version="+n+"1.0"+n+" encoding="+n+"utf-8"+n+"?>\n" #versi
                +"</pma:table>\n"
                +"</pma:database>\n"
                +"</pma:structure_schemas>\n"
-               +"<database name="+n+"bd1"+n+">\n"                           ####database name####
+               +"<database name="+n+name_database+n+">\n"##database name
               )
 ficheroA.close()
 
-##22222222222 llenado de tablas 
+##22222222222 llenado de tablas
 # peru (palabra(50),cuenta(50),fecha(50),comentario(250))
 #===============================================
 def busquedaPalabras(entrada,salida):

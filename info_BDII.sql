4fb3be9e19c6cd30081968c817be895468bb0faa
levesques <yonelyvan@gmail.com>
2014-05-06 07:47:23 -0500

    Create info_BDII.sql

---
+++ info_BDII.sql
@@ -0,0 +1,9 @@
+CREATE TABLE venta(id_venta     	int NOT NULL AUTO_INCREMENT, 
+                   cliente      	char(20) , 
+                   producto    		char(20) ,
+                   fecha_venta 	 	char(20) ,
+                   cantidad  		int      ,
+                   precio_unitario 	int  	 ,
+                   precio_total     int      ,
+                   PRIMARY KEY (id_venta)
+  				  );

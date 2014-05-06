9a8488d378e4b238fa0cf766a921c115b03bb1b0
levesques <yonelyvan@gmail.com>
2014-05-06 08:02:06 -0500

    Update info_BDII.sql

--- info_BDII.sql
+++ info_BDII.sql
@@ -1,12 +1,11 @@
 CREATE TABLE venta(id_venta     	int NOT NULL AUTO_INCREMENT, 
                    cliente      	char(20) , 
-                   producto    		char(20) ,
-                   fecha_venta 	 	char(20) ,
-                   cantidad  		int      ,
-                   precio_unitario 	int  	 ,
-                   precio_total     int      ,
+                   producto    	char(20) ,
+                   fecha_venta 	datetime ,
+                   cantidad  	int      ,
+                   precio_unitario 	int      ,
+                   precio_total       int      ,
                    PRIMARY KEY (id_venta)
                     );
-
 
 ALTER TABLE  `venta` ADD INDEX (  `cliente` ) ;

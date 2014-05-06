8f2489d1141827d84a8e3331eeca653d40c8681d
levesques <yonelyvan@gmail.com>
2014-05-06 08:16:21 -0500

    Update info_BDII.sql

--- info_BDII.sql
+++ info_BDII.sql
@@ -11,3 +11,12 @@ CREATE TABLE venta(id_venta     	int NOT NULL AUTO_INCREMENT,
 
 INSERT INTO `venta`( `cliente`, `producto`, `fecha_venta`, `cantidad`, `precio_unitario`, `precio_total`) 
 VALUES ('yonel','cuaderno','2013-01-01 00:00:00',1,13,13);
+
+CREATE FUNCTION get_fecha(cont int) RETURNS DATETIME
+RETURN DATE_ADD('2013-01-01 00:00:00', INTERVAL cont SECOND);
+
+//usando la la funcion get_fecha q aumenta en 120 segundos a una fecha inicial
+INSERT INTO `venta`( `cliente`, `producto`, `fecha_venta`, `cantidad`, `precio_unitario`, `precio_total`) 
+VALUES ('yonel','gato',get_fecha(120),1,20,20);
+
+

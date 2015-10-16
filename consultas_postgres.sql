423eee6f9e5e48d323613c314f472a495085abf0
yonelyvan <yonelyvan@gmail.com>
2015-10-16 00:21:49 -0500

    Update consultas_postgres.sql

--- consultas_postgres.sql
+++ consultas_postgres.sql
@@ -8,8 +8,8 @@ create table alumno(id int, nombre char(20), edad int, primary key(id));
 create table profesor(id int,nombre char(20), edad int, primary key(id));
 --inserts
 insert into alumno(id,nombre,edad) values(1,'yonel',21),(2,'ruben',23);
-insert into profesor(id,nombre,edad) values(1,'tejada',30),(2,'lovon',40);
+insert into profesor(id,nombre,edad) values(1,'prof1',30),(2,'prof2',40);
 --- querys
 select * from alumno;
-select * from profesor where nombre = 'lovon';
+select * from profesor where nombre = 'prof2';
 select id,edad  from alumno where id = 1;

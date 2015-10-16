bc743c787b651b9bdb4051542cc19acead377746
yonelyvan <yonelyvan@gmail.com>
2015-10-16 00:31:38 -0500

    Update consultas_postgres.sql

--- consultas_postgres.sql
+++ consultas_postgres.sql
@@ -13,3 +13,9 @@ insert into profesor(id,nombre,edad) values(1,'prof1',30),(2,'prof2',40);
 select * from alumno;
 select * from profesor where nombre = 'prof2';
 select id,edad  from alumno where id = 1;
+
+
+-- en directorio project/pgsql
+--     export PATH=$HOME/project/bin:$PATH
+--     export PGDATA=DemoDir
+--     initdb
